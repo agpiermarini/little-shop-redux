087cd897baee4b7c735d4a8553850155ff9510fd
agpiermarini <piermarini.andrew@gmail.com>
2018-03-13 21:09:13 -0600

    change to single quotes

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -8,7 +8,7 @@ describe 'Sinatra App' do
     LittleShopApp.new
   end
 
-  it "should allow accessing the home page" do
+  it 'should allow accessing the home page' do
     get '/'
 
     expect(last_response).to be_ok
