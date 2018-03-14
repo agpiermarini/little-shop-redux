0370bbac905e3540ef1d1dff02edf690fc5403fc
agpiermarini <piermarini.andrew@gmail.com>
2018-03-13 21:11:50 -0600

    change to single quotes

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,5 +1,5 @@
 class LittleShopApp < Sinatra::Base
-  set :root, File.expand_path("..", __dir__)
+  set :root, File.expand_path('..', __dir__)
 
   get '/' do
     erb :index
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Merchant do
   describe 'Validation' do
     it 'should be invalid to not have a name' do
       merchant_1 = Merchant.new()
-      merchant_2 = Merchant.new(name: "Turing")
+      merchant_2 = Merchant.new(name: 'Turing')
 
       expect(merchant_1).to_not be_valid
       expect(merchant_2).to be_valid
@@ -11,8 +11,8 @@ RSpec.describe Merchant do
 
   describe 'Create merchant' do
     it 'should add merchant to database' do
-      Merchant.create!(name: "One")
-      Merchant.create!(name: "Turing")
+      Merchant.create!(name: 'One')
+      Merchant.create!(name: 'Turing')
 
       expect(Merchant.find(1)).to be_an_instance_of(Merchant)
     end
@@ -20,37 +20,37 @@ RSpec.describe Merchant do
 
   describe 'Read merchant' do
     it 'can retrieve all merchants' do
-      Merchant.create!(name: "One")
-      Merchant.create!(name: "Turing")
+      Merchant.create!(name: 'One')
+      Merchant.create!(name: 'Turing')
 
       expect(Merchant.all.size).to eq(2)
     end
 
     it 'can retrieve merchant by id' do
-      Merchant.create!(name: "One")
-      Merchant.create!(name: "Turing")
+      Merchant.create!(name: 'One')
+      Merchant.create!(name: 'Turing')
 
-      expect(Merchant.find(1).name).to eq("One")
-      expect(Merchant.find(2).name).to eq("Turing")
+      expect(Merchant.find(1).name).to eq('One')
+      expect(Merchant.find(2).name).to eq('Turing')
     end
   end
 
   describe 'Update merchant' do
     it 'can change merchant name' do
-      Merchant.create!(name: "One")
+      Merchant.create!(name: 'One')
 
-      expect(Merchant.find(1).name).to eq("One")
+      expect(Merchant.find(1).name).to eq('One')
 
-      Merchant.find(1).update(name: "Two")
+      Merchant.find(1).update(name: 'Two')
 
-      expect(Merchant.find(1).name).to eq("Two")
+      expect(Merchant.find(1).name).to eq('Two')
     end
   end
 
   describe 'Delete merchant' do
     it 'should delete merchant' do
-      Merchant.create!(name: "One")
-      Merchant.create!(name: "Turing")
+      Merchant.create!(name: 'One')
+      Merchant.create!(name: 'Turing')
 
       expect(Merchant.all.size).to eq(2)
 
