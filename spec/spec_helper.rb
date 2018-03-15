51bf405c192f4f9c993c1506e32b7d39b4c08df5
agpiermarini <piermarini.andrew@gmail.com>
2018-03-15 13:39:59 -0600

    cleaned up spec_helper file and removed unecessary code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,18 +6,10 @@ ENV["RACK_ENV"] ||= "test"
 
 Bundler.require(:default, :test)
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec'
 require 'capybara/dsl'
-require 'rack/test'
-require 'sinatra'
 
 Capybara.app = LittleShopApp
 Capybara.save_path = 'tmp/capybara'
-
-RSpec.configure do |c|
-  c.include Capybara::DSL
-  c.include Rack::Test::Methods
-end
 
 DatabaseCleaner.strategy = :truncation
 
@@ -29,9 +21,5 @@ RSpec.configure do |c|
   c.after(:each) do
     DatabaseCleaner.clean
   end
-end
-
-module RSpecMixin
-  include Rack::Test::Methods
-  def app() Sinatra::Application end
+  c.include Capybara::DSL
 end
