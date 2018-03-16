c2522a0438bbc717264f557bc0da2b72a6f6c311
agpiermarini <piermarini.andrew@gmail.com>
2018-03-16 08:52:49 -0600

    removed merchant variable name

--- spec/features/invoices/user_visits_individual_invoice_page_spec.rb
+++ spec/features/invoices/user_visits_individual_invoice_page_spec.rb
@@ -1,7 +1,7 @@
 describe 'user visits individual invoice page' do
   it 'they see invoice_id and invoice status' do
     invoice = Invoice.create(merchant_id: 100, status: 'pending')
-    merchant = Merchant.create(id: 100, name: 'Boaty McBoatface')
+    Merchant.create(id: 100, name: 'Boaty McBoatface')
     visit "/invoices/#{invoice.id}"
     expect(page).to have_content "Invoice: #{invoice.id} - #{invoice.status}"
   end
