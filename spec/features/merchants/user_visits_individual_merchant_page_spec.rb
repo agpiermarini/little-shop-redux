56e1524a4973f4bfc72340c0a440c42557a81632
agpiermarini <piermarini.andrew@gmail.com>
2018-03-15 22:32:09 -0600

    appended _spec to user_visits_individual_merchant_page.rb so that rspec would hit tests, and corrected failing tests in merchant/show.rb--also changed click_button to click_link in test for edit button

--- spec/features/merchants/user_visits_individual_merchant_page_spec.rb
+++ spec/features/merchants/user_visits_individual_merchant_page_spec.rb
@@ -1,5 +1,3 @@
-require './app/controllers/little_shop_app'
-
 describe 'user visits individual merchant page' do
   context 'they click on delete merchant button' do
     it 'they delete a merchant' do
@@ -17,9 +15,9 @@ describe 'user visits individual merchant page' do
     it 'they are redirected to /merchant/:id/edit' do
       Merchant.create(name: 'Meeeeeee', id: 3)
       visit '/merchants/3'
-      click_button 'Edit'
+      click_link 'Edit'
 
-      current_path.should == '/merchants/3/edit'
+      expect(current_path).to eq '/merchants/3/edit'
     end
   end
 end
