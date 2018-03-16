226cdd904d9b02e7cdbf67ce9e5b8e1e8f8d7bd1
agpiermarini <piermarini.andrew@gmail.com>
2018-03-15 18:54:51 -0600

    revised individual_invoice_page feature test to user interpolation for invoice id

--- spec/features/invoices/user_visits_individual_invoice_page.rb
+++ spec/features/invoices/user_visits_individual_invoice_page.rb
@@ -3,6 +3,5 @@ describe 'user visits individual invoice page' do
     invoice = Invoice.create(merchant_id: 100, status: 'pending')
     visit 'invoices/<%= invoice.id %>'
     expect(page).to have_content "Invoice: <%= invoice.id %> - <%= invoice.status %>"
-
   end
 end
