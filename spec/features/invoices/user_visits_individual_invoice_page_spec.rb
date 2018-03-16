d20e00faade2d6f1266e456d14334f18d8405803
agpiermarini <piermarini.andrew@gmail.com>
2018-03-16 08:43:40 -0600

    changes to get paths to correct errors

--- spec/features/invoices/user_visits_individual_invoice_page_spec.rb
+++ spec/features/invoices/user_visits_individual_invoice_page_spec.rb
@@ -2,7 +2,7 @@ describe 'user visits individual invoice page' do
   it 'they see invoice_id and invoice status' do
     invoice = Invoice.create(merchant_id: 100, status: 'pending')
     merchant = Merchant.create(id: 100, name: 'Boaty McBoatface')
-    visit "invoices/#{invoice.id}"
+    visit "/invoices/#{invoice.id}"
     expect(page).to have_content "Invoice: #{invoice.id} - #{invoice.status}"
   end
 end
@@ -11,7 +11,7 @@ describe 'test' do
   it 'they see merchant name' do
     invoice = Invoice.create(merchant_id: 100, status: 'pending')
     merchant = Merchant.create(id: 100, name: 'Boaty McBoatface')
-    visit "invoices/#{invoice.id}"
+    visit "/invoices/#{invoice.id}"
     expect(page).to have_content "#{merchant.name}"
   end
 end
