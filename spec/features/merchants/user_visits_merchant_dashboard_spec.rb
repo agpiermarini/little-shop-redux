bf48099b0f38ffcfc94bc7f6dbfffe11dd4879e2
Megan Arellano <ssciolist@gmail.com>
2018-03-18 12:59:31 -0600

    more view

--- spec/features/merchants/user_visits_merchant_dashboard_spec.rb
+++ spec/features/merchants/user_visits_merchant_dashboard_spec.rb
@@ -19,7 +19,7 @@ describe 'user visits merchant dashboard' do
                           price: 150,
                           image: 'Picture of low profile shoes made of canvas',
                           merchant_id: 3)
-      item = Item.create!(title: 'High tops',
+      item4 = Item.create!(title: 'High tops',
                           description: 'Above ankle shoes made of canvas',
                           price: 200,
                           image: 'Picture of above ankle shoes made of canvas',
@@ -52,7 +52,7 @@ describe 'user visits merchant dashboard' do
                           price: 150,
                           image: 'Picture of low profile shoes made of canvas',
                           merchant_id: 3)
-      item = Item.create!(title: 'High tops',
+      item4 = Item.create!(title: 'High tops',
                           description: 'Above ankle shoes made of canvas',
                           price: 200,
                           image: 'Picture of above ankle shoes made of canvas',
@@ -62,6 +62,41 @@ describe 'user visits merchant dashboard' do
 
       within(:css, "#highest-price")
       expect(page).to have_content("Vans")
+    end
+  end
+
+  context "they see an individual merchant's analytics" do
+    it "shows an individual merchant's item count" do
+      merchant = Merchant.create(name: 'Crocs')
+      item1 = Item.create!(title: 'Sandals',
+                          description: 'Shoes with holes',
+                          price: 50,
+                          image: 'Picture of shoes with holes',
+                          merchant_id: 1)
+      item2 = Item.create!(title: 'Heels',
+                          description: 'Plastic shoes with holes',
+                          price: 100,
+                          image: 'Picture of shoes with knit fabric',
+                          merchant_id: 1)
+      item3 = Item.create!(title: 'Sneakers',
+                          description: 'Very ugly shoes',
+                          price: 150,
+                          image: 'Picture of very ugly shoes',
+                          merchant_id: 1)
+      item4 = Item.create!(title: 'Flip Flops',
+                          description: 'Why do these exist',
+                          price: 200,
+                          image: 'Picture of tremendously ugly shoes',
+                          merchant_id: 1)
+
+      within(:css, "#merchant-id-#{merchant.id}")
+      expect(page).to have_content("Item Count: 4")
+    end
+
+    skip "shows an individual merchant's average item price" do
+    end
+
+    skip "shows individual merchant's total cost of items" do
     end
   end
 end
