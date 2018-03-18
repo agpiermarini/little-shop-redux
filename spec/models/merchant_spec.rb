d5516e1324e215af49101ce365f68c9534281fa7
Megan Arellano <ssciolist@gmail.com>
2018-03-18 13:33:24 -0600

    add merchant model class method tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -93,3 +93,65 @@ describe 'Instance Methods' do
     end
   end
 end
+
+describe 'Class Methods' do
+  describe '.most_items' do
+    it 'finds the merchant with the most items' do
+      merchant1 = Merchant.create(name: 'Crocs')
+      merchant2 = Merchant.create(name: 'Nike')
+      merchant3 = Merchant.create(name: 'Vans')
+      item1 = Item.create!(title: 'Sandals',
+                          description: 'Shoes with holes',
+                          price: 50,
+                          image: 'Picture of shoes with holes',
+                          merchant_id: 1)
+      item2 = Item.create!(title: 'FlyKnit',
+                          description: 'Shoes with knit fabric',
+                          price: 100,
+                          image: 'Picture of shoes with knit fabric',
+                          merchant_id: 2)
+      item3 = Item.create!(title: 'Canvas shoes',
+                          description: 'Low profile shoes made of canvas',
+                          price: 150,
+                          image: 'Picture of low profile shoes made of canvas',
+                          merchant_id: 3)
+      item4 = Item.create!(title: 'High tops',
+                          description: 'Above ankle shoes made of canvas',
+                          price: 200,
+                          image: 'Picture of above ankle shoes made of canvas',
+                          merchant_id: 3)
+
+      expect(Merchant.most_items).to eq(merchant3)
+    end
+  end
+
+  describe '.highest_price_item' do
+    it 'finds the merchant with the highest priced item' do
+      merchant = Merchant.create(name: 'Crocs')
+      merchant = Merchant.create(name: 'Nike')
+      merchant = Merchant.create(name: 'Vans')
+      item1 = Item.create!(title: 'Sandals',
+                          description: 'Shoes with holes',
+                          price: 50,
+                          image: 'Picture of shoes with holes',
+                          merchant_id: 1)
+      item2 = Item.create!(title: 'FlyKnit',
+                          description: 'Shoes with knit fabric',
+                          price: 100,
+                          image: 'Picture of shoes with knit fabric',
+                          merchant_id: 2)
+      item3 = Item.create!(title: 'Canvas shoes',
+                          description: 'Low profile shoes made of canvas',
+                          price: 150,
+                          image: 'Picture of low profile shoes made of canvas',
+                          merchant_id: 3)
+      item4 = Item.create!(title: 'High tops',
+                          description: 'Above ankle shoes made of canvas',
+                          price: 200,
+                          image: 'Picture of above ankle shoes made of canvas',
+                          merchant_id: 3)
+
+      expect(Merchant.highest_price_item).to eq(merchant3)
+    end
+  end
+end
