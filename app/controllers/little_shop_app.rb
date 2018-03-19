e68b402ed9e0e39775816aaa591928dc4aafde0c
agpiermarini <piermarini.andrew@gmail.com>
2018-03-19 16:40:38 -0600

    added path for invoices dashboard, revised controller to pass locals variable to the items dashboard and made corresponding changes to items/dashboard.erb

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -135,4 +135,18 @@ class LittleShopApp < Sinatra::Base
   get '/items-dashboard' do
     erb :'items/dashboard'
   end
+
+  get '/items-dashboard' do
+    erb :'items/dashboard',
+    :locals => {
+      :items => Item.all
+    }
+  end
+
+  get '/items-dashboard' do
+    erb :'invoices/dashboard',
+    :locals => {
+      :invoices => Invoice.all
+    }
+  end
 end
